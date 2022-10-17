eba9825546ef1f41c01382353c7bcb94227c3951
Ajah John Nnaemeka <ajahjohn80@gmail.com>
2022-10-17 23:40:58 +0000

    New compiled program

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * main - main program entry point
+ * main - the program entry point
  *
- * Return:0 if no error, non zero if error.
+ * Return: Return:0 if no error, non zero if error.
  */
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
