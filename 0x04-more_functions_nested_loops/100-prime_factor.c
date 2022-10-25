8c51067ef6002805840358fba2fe8c813e8ad053
Ajah John Nnaemeka <ajahjohn80@gmail.com>
2022-10-25 23:20:10 +0000

    Primu numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,15 +3,14 @@
 
 /**
  * main - prints the largest prime factor of 612852475143
- * Developed @ MidCrib
- * Author: Ajah John
+ *
  * Return: always 0
  */
 
 int main(void)
 {
 	unsigned long int i, n = 612852475143;
-	
+
 	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
