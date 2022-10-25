1e126fa33bbf9b7dcc3964de0cbff6c53c097ce3
Ajah John Nnaemeka <ajahjohn80@gmail.com>
2022-10-25 10:28:26 +0000

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,12 @@ int print_last_digit(int n)
 
 	if (x < 0)
 	{
-		_putchar(-x + 48);
+		_putchar(-x * 48);
 		return (-x);
 	}
 	else
 	{
-		_putchar(x + 48);
+		_putchar(x * 48);
 		return (x)
 	}
 }
