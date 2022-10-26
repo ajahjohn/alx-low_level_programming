679a3e4de277fe1acfc7467763b01a9c3c202ef3
Ajah John Nnaemeka <ajahjohn80@gmail.com>
2022-10-26 11:44:55 +0000

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,10 +14,13 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)*/
-	/* {*/
-	/* putchar(i);*/
-	/* }*/
+	/*
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 *	i++;
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
